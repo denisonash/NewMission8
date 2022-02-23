49f034ec6080e9224759eca69d20b18ae0132260
ashleydenison <dashley9@byu.edu>
2022-02-23 14:37:06 -0700

    Final commit

---
+++ Mission7/Components/CategoryViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Mission7.Models;
+
+namespace Mission7.Components
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private IBookstoreRepository repo { get; set; }
+
+        public CategoryViewComponent (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+
+            var categories = repo.Books
+                .Select(c => c.Category)
+                .Distinct()
+                .OrderBy(c => c);
+
+            return View(categories);
+        }
+    }
+}
