2c5a7c4102c710730d769909e7cb1aa35619b7f9
ashleydenison <dashley9@byu.edu>
2022-02-23 11:50:54 -0700

    Initial Commit

---
+++ Mission7/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission7.Models;
+using Mission7.Models.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Mission7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        // GET: /<controller>/
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                    .OrderBy(b => b.Title)
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+    }
+}
---
+++ Mission7/Models/BookstoreContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission7.Models
+{
+    public class BookstoreContext : DbContext
+    {
+        public BookstoreContext()
+        {
+
+        }
+
+        public BookstoreContext(DbContextOptions<BookstoreContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Books> Books { get; set; }
+    }
+}
